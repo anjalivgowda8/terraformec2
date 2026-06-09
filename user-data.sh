0d0e4d600325d41219fa9b0309ecb809335cbf11
Anjali V <anjalivgowda8@gmail.com>
2026-06-09 00:48:04 -0700

    auto mate nginx project

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+set -e
+
 apt update -y
-apt install -y docker.io git
+apt install -y docker.io docker-compose-v2 git
+
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ubuntu
+
+usermod -aG docker ubuntu || true
+
+# Go to home directory
+cd /home/ubuntu
+
+# Clone repo
+if [ ! -d "nginx-rotate-app-" ]; then
+  git clone https://github.com/anjalivgowda8/nginx-rotate-app-.git
+fi
+
+cd nginx-rotate-app-
+
+# Build and start containers
+docker compose down || true
+docker compose up -d --build
